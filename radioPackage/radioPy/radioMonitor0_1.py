8755214d2c99996b80bf2bd399a749f02cadf74e
sbgraves237 <spencer.graves@effectivedefense.org>
2018-10-28 14:32:23 -0500

    add radioMonitor0_1.py

---
+++ radioPackage/radioPy/radioMonitor0_1.py
@@ -0,0 +1,67 @@
+def radioMonitor0_1(
+  source="90_1FM", 
+  RECORD_SECONDS=5, 
+  
+  ):
+  """Function to record a live stream to a series of """
+  """*.wav files in an infinite loop, """
+  """subject to manual termination."""
+  
+  """generalizing:  """
+  """PyAudio example: Record a few seconds of audio and save to a WAVE file."""
+  """based in part on""" 
+  """https://stackoverflow.com/questions/35344649/reading-input-sound-signal-using-python"""
+  
+  """The series should start at the time the software is called,"""
+  """with each file carrying the start time in its name"""
+  """and ending the observation on the next integer multiple"""
+  """of RECORD_SECONDS.  """
+
+  import pyaudio
+  import wave
+
+  CHUNK = 1024
+  FORMAT = pyaudio.paInt16
+  CHANNELS = 2
+  RATE = 44100
+# RECORD_SECONDS = 5
+# In R I would write:  
+#   SysTm <- Sys.time()
+#   SysTme <- past0(substring(SysTm, 1, 10), 
+#       'T', substring(SysTm, 12, 19))
+#   SysTime <- gsub(':', '_', SysTme)
+# WAVE_OUTPUT_FILENAME <- paste0(source, 
+#   SysTime, ".wav")
+  WAVE_OUTPUT_FILENAME = "KKFI2018-10-12T13_08-5sec.wav"
+#   
+
+  p = pyaudio.PyAudio()
+
+  stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+  print("* recording")
+
+  frames = []
+
+  for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+  print("* done recording")
+
+  stream.stop_stream()
+  stream.close()
+  p.terminate()
+
+  wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+  wf.setnchannels(CHANNELS)
+  wf.setsampwidth(p.get_sample_size(FORMAT))
+  wf.setframerate(RATE)
+  wf.writeframes(b''.join(frames))
+  wf.close()
+
+
