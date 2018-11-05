751744e57380cdfbfb5b553dbf19019ea9e0a163
sbgraves237 <spencer.graves@effectivedefense.org>
2018-11-04 22:20:00 -0600

    test script for mk_source_dir

---
+++ tst_mk_source_dir.py
@@ -0,0 +1,6 @@
+from radioPackage.radioPy import mk_source_dir 
+
+mk_source_dir.mk_source_dir("pre", "1", 2)
+mk_source_dir.mk_source_dir("pre", "2", 2)
+mk_source_dir.mk_source_dir("pre", "3", 2)
+
